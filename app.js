0925f1b1f1de4d1ed23b42405076205b3c62d8db
Omar A. Orga <orgaomar8@gmail.com>
2023-03-18 22:58:42 +0800

    Add function to calculator

--- app.js
+++ app.js
@@ -1,20 +1,74 @@
+const resultContainer = document.querySelector(`.result`);
+const lastResultContainer = document.querySelector(`.last-resultContainer`);
 
+function clearDisplay() {
+  resultContainer.innerHTML = ``;
+  lastResultContainer.innerHTML = ``;
+}
 
+function deleteLastChar() {
+  resultContainer.innerHTML = resultContainer.innerHTML.substring(
+    0,
+    resultContainer.innerHTML.length - 1
+  );
+}
 
+function appendValue(str) {
+  resultContainer.innerHTML += str;
 
+pointCounter = resultContainer.innerHTML.match(/\D/g);
+objectLength = Object.keys(pointCounter).length;
+if(pointCounter[objectLength-1] === `.` && pointCounter[objectLength-2] === `.`){
+    deleteLastChar();
+}
 
+  lastTwoStr = resultContainer.innerHTML.substring(
+    resultContainer.innerHTML.length - 2,
+    resultContainer.innerHTML.length
+  );
+ 
+  lastThreeStr = resultContainer.innerHTML.substring(
+    resultContainer.innerHTML.length - 3,
+    resultContainer.innerHTML.length
+  );
+  if (
+    lastTwoStr === `..` ||
+    lastTwoStr === `**` ||
+    lastTwoStr === `//` ||
+    lastTwoStr === `²²` ||
+    lastTwoStr === `%%`
+  ) {
+    deleteLastChar();
+  }
+  if (lastThreeStr === `+++` || lastThreeStr === `---`) {
+    deleteLastChar();
+  }
+}
 
-// function add(a,b){
-//     return a+b;
-// }
-// function subtract(a,b){
-//     return a-b;
-// }
-// function multiply(a,b){
-//     return a*b;
-// }
-// function divide(a,b){
-//     return a/b;
-// }
-
+function calculate() {
+  operation = resultContainer.innerHTML.match(/[\+\-\/\*\%]/);
+  index = resultContainer.innerHTML.search(/[\+\-\/\*\%]/);
+  num1 = parseFloat(resultContainer.innerHTML.slice(0, index));
+  num2 = parseFloat(resultContainer.innerHTML.slice(index + 1));
+  console.log(operation[0])
+  var result;
+  switch (operation[0]) {
+    case `*`:
+        result = num1 * num2;
+        resultContainer.innerHTML = result;
+        break;
+    case `/`:
+        result = num1 / num2;
+        resultContainer.innerHTML = result;
+        break;
+    case `+`:
+        result = num1 + num2;
+        resultContainer.innerHTML = result;
+        break;
+    case `-`:
+        result = num1 - num2;
+        resultContainer.innerHTML = result;
+        break;
+  }
+}
 
