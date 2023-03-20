d0aaa67518987cb33552b3cd21e0723782f94b2b
Omar A. Orga <orgaomar8@gmail.com>
2023-03-20 13:54:26 +0800

    Add function to not evaluate when no num2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const resultContainer = document.querySelector(`.result`);
 const lastResultContainer = document.querySelector(`.last-resultContainer`);
+let operated = false;
 
 function clearDisplay() {
   resultContainer.innerHTML = ``;
@@ -7,27 +8,28 @@ function clearDisplay() {
 }
 
 function deleteLastChar() {
-  lastStr = resultContainer.innerHTML.substring(resultContainer.innerHTML.length-1, resultContainer.innerHTML.length);
-  console.log(lastStr)
-  if(lastStr===` `){
+  lastStr = resultContainer.innerHTML.substring(
+    resultContainer.innerHTML.length - 1,
+    resultContainer.innerHTML.length
+  );
+  console.log(lastStr);
+  if (lastStr === ` `) {
     resultContainer.innerHTML = resultContainer.innerHTML.substring(
       0,
       resultContainer.innerHTML.length - 3
     );
-  } else if (resultContainer.innerHTML===`Undefined 😡`){
+  } else if (resultContainer.innerHTML === `Undefined 😡`) {
     clearDisplay();
-  }
-  else{
-  resultContainer.innerHTML = resultContainer.innerHTML.substring(
-    0,
-    resultContainer.innerHTML.length - 1
-  );
+  } else {
+    resultContainer.innerHTML = resultContainer.innerHTML.substring(
+      0,
+      resultContainer.innerHTML.length - 1
+    );
   }
 }
 
 function changeSign() {
   entries = resultContainer.innerHTML.split(` `);
-  console.log(entries);
   newNumber = entries[entries.length - 1] * -1;
   entries[entries.length - 1] = newNumber;
   resultContainer.innerHTML = entries.join(` `);
@@ -37,7 +39,7 @@ function appendValue(str) {
   resultContainer.innerHTML += str;
 
   entries = resultContainer.innerHTML.split(` `);
-  if(Object.keys(entries).length>3){
+  if (Object.keys(entries).length > 3) {
     deleteLastChar();
   }
 
@@ -54,7 +56,6 @@ function appendValue(str) {
     resultContainer.innerHTML.length - 6,
     resultContainer.innerHTML.length
   );
-  console.log(lastSixStr)
 
   if (
     lastSixStr === ` +  + ` ||
@@ -70,39 +71,61 @@ function appendValue(str) {
   }
 }
 
-function calculate() {
+function operate() {
   entries = resultContainer.innerHTML.split(` `);
   num1 = parseFloat(entries[0]);
   num2 = parseFloat(entries[2]);
   operation = entries[1];
-  console.log(num1);
-  console.log(num2);
-  console.log(operation);
   let result;
   switch (operation) {
     case `*`:
       result = num1 * num2;
       resultContainer.innerHTML = result;
+      operated = true;
+      if(entries[2]===``){
+        resultContainer.innerHTML = entries.join(` `)
+        operated = false;
+      }
       break;
     case `/`:
-      if(num2===0){
-        resultContainer.innerHTML = `Undefined 😡`
-      }else{
+      if (num2 === 0) {
+        resultContainer.innerHTML = `Undefined 😡`;
+        operated = false;
+      }else if(entries[2]===``){
+        resultContainer.innerHTML = entries.join(` `)
+        operated = false;
+      }else {
         result = num1 / num2;
         resultContainer.innerHTML = result;
+        operated = true;
       }
       break;
     case `+`:
       result = num1 + num2;
       resultContainer.innerHTML = result;
+      operated = true;
+      if(entries[2]===``){
+        resultContainer.innerHTML = entries.join(` `)
+        operated = false;
+      }
       break;
     case `-`:
       result = num1 - num2;
       resultContainer.innerHTML = result;
+      operated = true;
+      if(entries[2]===``){
+        resultContainer.innerHTML = entries.join(` `)
+        operated = false;
+      }
       break;
     case `%`:
       result = num1 % num2;
       resultContainer.innerHTML = result;
+      operated = true;
+      if(entries[2]===``){
+        resultContainer.innerHTML = entries.join(` `)
+        operated = false;
+      }
       break;
   }
 }
